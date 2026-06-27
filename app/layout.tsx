3232c480058d5359b7473c5ee3e41e6eb16c84bd
cgcartaya <carlillo8909@gmail.com>
2026-06-27 18:55:37 -0400

    arreglando imagen whatsapp

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,15 +58,14 @@ export const metadata: Metadata = {
       "Envíos, compras y entregas desde Miami hacia Cienfuegos, Cuba.",
     url: "https://aguilacubaexpress.com",
     siteName: "Águila Cuba Express",
-    images: [
-      {
-        url: "https://aguilacubaexpress.com/og-image.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Águila Cuba Express",
-        type: "image/jpeg",
-      },
-    ],
+   images: [
+  {
+    url: "/og-image.jpg",
+    width: 1200,
+    height: 630,
+    alt: "Águila Cuba Express",
+  },
+],
     locale: "es_US",
     type: "website",
   },
