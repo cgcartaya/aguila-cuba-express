041ffab791d77c1ab3405b415016ad6304397ef0
cgcartaya <117411542+cgcartaya@users.noreply.github.com>
2026-09-22 10:16:37 -0400

    Add province-aware delivery zone foundation

---
+++ lib/checkout/cuba-provinces.ts
@@ -0,0 +1,2 @@
+export const CUBA_PROVINCES = ["Pinar del Río","Artemisa","La Habana","Mayabeque","Matanzas","Cienfuegos","Villa Clara","Sancti Spíritus","Ciego de Ávila","Camagüey","Las Tunas","Holguín","Granma","Santiago de Cuba","Guantánamo","Isla de la Juventud"] as const;
+export const DEFAULT_CUBA_PROVINCE = "Cienfuegos";
